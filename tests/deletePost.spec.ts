c553c0b67b31a5d997daac615573fb9475ed13c8
Edielton Dantas <edielton.dantas@anixe.pl>
2020-11-09 13:00:24 +0100

    Add test to deletePost function

---
+++ tests/deletePost.spec.ts
@@ -0,0 +1,35 @@
+import * as httpMocks from 'node-mocks-http';
+import {deletePost, url} from '../src/index';
+import {mocked} from 'ts-jest/utils';
+import axios from 'axios';
+
+jest.mock('axios');
+
+const DEEP_MOCK = true;
+const mockedAxios = mocked(axios, DEEP_MOCK);
+
+describe('getPost', () => {
+  afterAll(()=> {
+    jest.clearAllMocks();
+    mockedAxios.mockClear();
+  });
+
+  it('should delete post', async () => {
+    const POST_ID = 1;
+    const req = httpMocks.createRequest({
+      params: {
+        '0': POST_ID,
+      },
+    });
+    const res = httpMocks.createResponse();
+
+    mockedAxios.delete.mockResolvedValue(undefined);
+    res.send = jest.fn();
+
+    await deletePost(req, res);
+
+    expect(res.send).toHaveBeenCalled();
+    expect(mockedAxios.delete).toHaveBeenLastCalledWith(`${url}/${POST_ID}`);
+    expect(res.statusCode).toBe(202);
+  });
+});
