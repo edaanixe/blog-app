7b6209a8cd18287de6b92c86a1dc910a828c9fd3
Edielton Dantas <edielton.dantas@anixe.pl>
2020-11-09 07:33:08 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import * as functions from 'firebase-functions';
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+export const getPosts = functions.https.onRequest(async (req, res) => {
+  try {
+    const {data: posts} = await axios.get(url);
+
+    res.send(posts);
+  } catch ({response}) {
+    functions.logger.error(response.statusText, {structuredData: true});
+    res.status(response.status).send(response.statusText);
+  }
+});
+
+export const getPost = functions.https.onRequest(async (req, res) => {
+  try {
+    const postId = req.params[0];
+    const {data: post} = await axios.get(`${url}/${postId}`);
+
+    res.send(post);
+  } catch ({response}) {
+    functions.logger.error(response.statusText, {structuredData: true});
+    res.status(response.status).send(response.statusText);
+  }
+});
+
+export const createPost = functions.https.onRequest(async (req, res) => {
+  try {
+    const {
+      userId,
+      title,
+      body,
+    } = req.body;
+
+    const {data: post} = await axios.post(url, {userId, title, body});
+
+    res.status(201).send(post);
+  } catch ({response}) {
+    functions.logger.error(response.statusText, {structuredData: true});
+    res.status(response.status).send(response.statusText);
+  }
+});
+
+export const updatePost = functions.https.onRequest(async (req, res) => {
+  try {
+    const postId = req.params[0];
+    const {
+      title,
+      body,
+    } = req.body;
+
+    await axios.put(`${url}/${postId}`, {title, body});
+
+    res.status(204).send();
+  } catch ({response}) {
+    functions.logger.error(response.statusText, {structuredData: true});
+    res.status(response.status).send(response.statusText);
+  }
+});
+
+export const deletePost = functions.https.onRequest(async (req, res) => {
+  try {
+    const postId = req.params[0];
+
+    await axios.delete(`${url}/${postId}`);
+
+    res.status(202).send();
+  } catch ({response}) {
+    functions.logger.error(response.statusText, {structuredData: true});
+    res.status(response.status).send(response.statusText);
+  }
+});
